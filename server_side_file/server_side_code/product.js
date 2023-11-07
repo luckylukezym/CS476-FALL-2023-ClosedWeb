8a8bb5d7b4318a809e8627dd05138a90420a75a5
Yuanfeng Xin <42011577+xin205@users.noreply.github.com>
2023-11-06 21:29:47 -0600

    Delete server_side_file directory

--- server_side_file/server_side_code/product.js
+++
@@ -1,6 +0,0 @@
-let add_button = document.querySelector(".add-button");
-let title = document.querySelector(".product-title");
-
-add_button.addEventListener("click", function (e) {
-  window.location.href = `add_button_process.php?name=${title.textContent}`;
-});

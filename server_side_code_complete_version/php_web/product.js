78968b5c43952fe3f47703c861803afe046769e2
Yuanfeng Xin <42011577+xin205@users.noreply.github.com>
2023-11-13 16:24:06 -0600

    Update product.js

--- server_side_code_complete_version/php_web/product.js
+++ server_side_code_complete_version/php_web/product.js
@@ -2,5 +2,11 @@ let add_button = document.querySelector(".add-button");
 let title = document.querySelector(".product-title");
 
 add_button.addEventListener("click", function (e) {
+  alert("product added to cart");
+  setTimeout(alert_user, "2000");
   window.location.href = `add_button_process.php?name=${title.textContent}`;
 });
+
+function alert_user() {
+  alert("product added to cart.");
+}
